9664ada9093ecd4b41e4ae4200909b76209ceea6
143KRISHU <1712rishabhofficial1997@gmail.com>
2023-10-04 00:56:04 +0530

    modified membership.py

--- membership.py
+++ membership.py
@@ -1,4 +1,4 @@
 # Membership Operator : in and not in
-city =["kanpur", "delhi", "allahbad", "manali"]
+city = ["kanpur", "delhi", "allahbad", "manali"]
 print("manali" in city)
 print("manali" not in city)
